143a2c22eb11ab119d6fd6b0defc48e973671e52
Rein <reinX244@gmail.com>
2023-02-21 18:03:22 +0800

    Added HelloWorld & AskNameAndAge, for setting up purposes only

---
+++ 02-21-23/AskNameAndAge.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class AskNameAndAge {
+
+   public static void main(String[] args) {
+      // Creating a Scanner object to accept user input
+      Scanner userInput = new Scanner(System.in);
+      // Ask for name
+      System.out.print("Enter your name: ");
+      String userName = userInput.nextLine();
+      // Ask for age
+      System.out.print("Enter your age: ");
+      int userAge = userInput.nextInt();
+      // Printing userName & userAge
+      System.out.printf("[Name\t:\t%s]\n[Age\t:\t%d]\n", userName, userAge);
+      
+   }
+
+}
---
+++ 02-21-23/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+   public static void main(String[] args) {
+      // Ctrl + B to compile
+      // Ctrl + R to run
+      System.out.println("Hello, World!");
+   }
+
+}
