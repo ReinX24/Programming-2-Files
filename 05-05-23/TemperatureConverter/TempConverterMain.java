fc22ccda09a2e5436bc69720e5c442b84ecd7711
Rein <reinX244@gmail.com>
2023-05-05 14:57:09 +0800

    Added TemperatureConverter, still in progress

---
+++ 05-05-23/TemperatureConverter/TempConverterMain.java
@@ -0,0 +1,14 @@
+public class TempConverterMain {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Creating a Java Swing program that converts the temperature from celsius to
+		 * fahrenheit and vice versa
+		 */
+
+		new TempConverterFrame();
+
+	}
+
+}
